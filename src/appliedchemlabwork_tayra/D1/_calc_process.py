3042ebd4ae3f0cae960980bf08fb654f1558fb63
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-06-06 21:20:21 +0900

    Documentation changes.

--- src/appliedchemlabwork_tayra/D1/_calc_process.py
+++ src/appliedchemlabwork_tayra/D1/_calc_process.py
@@ -218,7 +218,6 @@ def calc_left(
     -----
     The value :math:`v` is equivalent to
     .. math::
-
        v ={} \\ln \\frac{a \\left( b -{} x \\right)}{b \\left( a -{} x \\right)}
     """
     up = a * (b - x)
